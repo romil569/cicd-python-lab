b6e08505c4bb355c0120e4f30acd1eba2463c9f3
romil569 <romilparikh569@gmail.com>
2026-03-25 09:42:15 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1>CI/CD Pipeline is Live!</h1><p>Version 1.0</p>"
+    return "<h1>CI/CD Pipeline is Live!</h1><p>Version 2.0: Automated Update Successful!</p>"
 
 if __name__ == '__main__':
     application.run()
